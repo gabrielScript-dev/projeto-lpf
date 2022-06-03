15efecb376758badf56560a736805b2269f26272
gabrielScript-dev <gabriel.script@gmail.com>
2022-06-03 18:53:22 -0300

    Update script.kt

--- script.kt
+++ script.kt
@@ -2,136 +2,165 @@ import kotlinx.browser.*
 import org.w3c.dom.*
 import kotlin.random.Random
 
-var rodada = 0
-var chances = 5
-var pontucaoTotal = 0
-var palavraSecreta: String = palavras()
-
+val partida: Partida = Partida(3)
 val area = document.getElementById("palavras") 
 
-fun palavras() : String {
+class Palavra() {
+    private var palavraSecreta: String
 
-    val palavras : Array<String> = arrayOf("mouse", "sagaz", "mexer", "termo","senso","nobre","algoz","afeto", "ponto",
-    "plena","sutil","vigor","fazer","audaz","sanar","assim","inato","cerne","ideia","fosse", "round", "abrir", "hiato",
-    "desde","poder","moral","torpe","muito","honra","justo","gozar", "anexo","etnia", "sobre", "sonho", "tange", "lapso", 
-    "expor", "haver", "amigo", "carma", "velho", "sonsa", "ideal", "claro", "doido", "horda", "inata", "capaz", "xeque")
-    val random = Random.nextInt(until = palavras.size)
-	console.log(palavras[random])
-    return palavras[random]
+    fun palavras() : String {
 
+        val palavras : Array<String> = arrayOf("mouse", "sagaz", "mexer", "termo","senso","nobre","algoz","afeto", "ponto",
+        "plena","sutil","vigor","fazer","audaz","sanar","assim","inato","cerne","ideia","fosse", "round", "abrir", "hiato",
+        "desde","poder","moral","torpe","muito","honra","justo","gozar", "anexo","etnia", "sobre", "sonho", "tange", "lapso", 
+        "expor", "haver", "amigo", "carma", "velho", "sonsa", "ideal", "claro", "doido", "horda", "inata", "capaz", "xeque")
+        val random = Random.nextInt(until = palavras.size)
+        console.log(palavras[random])
+        return palavras[random]
+
+    }  
+
+    fun trocarPalavra() {
+        this.palavraSecreta = palavras()
+    } 
+
+    fun temLetrasEmComum(s: String): String {
+
+        val palavraComMarcacao = s.mapIndexed {i, x -> 
+            if(x == palavraSecreta[i]) {
+                //na ordem certa
+                "<span class=\"green\">" + x + "</span>"
+            } else if(palavraSecreta.contains(x)) {
+                //fora de ordem
+                "<span class=\"yellow\">" + x + "</span>"
+            } else { 
+                "<span>" + x + "</span>"
+            }}.toString().replace(",", " ")
+
+        return "<div class=\"jogadas\"><h1> " + palavraComMarcacao.uppercase() + "</h1></div>"
+    }
+
+    fun ehIgual(palavraEscolhida: String): Boolean {
+	    return palavraEscolhida.equals(this.palavraSecreta)
+    }   
+
+    init {
+        this.palavraSecreta = palavras()
+    }
 }
 
-fun proximaRodada(pontuacaoDaRodada: Int) {
-	palavraSecreta = palavras()
-	pontucaoTotal += pontuacaoDaRodada
-	chances = 5
-	rodada++
+class Partida constructor(qtdRodadas: Int) {
+    var rodadas: Int
+    var chances: Int
+    var pontuacaoTotal: Int
+    private val palavraSecreta: Palavra
+    
+    fun proximaRodada(pontuacaoDaRodada: Int) {
+        this.pontuacaoTotal += pontuacaoDaRodada
+        if(this.rodadas > 0) {
+	        this.palavraSecreta.trocarPalavra()
+	        this.chances = 5
+	    	this.rodadas--
+        }
+
+        console.log(pontuacaoDaRodada)
+    }
+
+    fun acertou(acertou: Boolean): String {
+        return if(acertou) "<h1>Você acertou!</h1>" else "<h1>Você errou!</h1>"
+    }
+
+    fun palavraModificada(palavra: String): String {
+        return palavraSecreta.temLetrasEmComum(palavra)
+    }
+
+    fun verificarPalavra(s: String): Boolean {
+
+        val resultado = palavraSecreta.ehIgual(s)
+        return resultado
+    }
+
+    fun fimDeJogo(): String {
+        return 
+		"""
+            <h1> Game Over! </h1>
+            <form method="POST" action="salvar_pontuacao">
+                Nome: <input name="nome_usuario" type="text">
+                <input name ="pontos" type="hidden" value="${this.pontuacaoTotal}"> 
+                <input type="submit" value="Enviar">
+            </form>
+        """
+    }
+
+    init {
+        this.chances = 5
+        this.pontuacaoTotal = 0
+        this.rodadas = qtdRodadas
+        this.palavraSecreta = Palavra()
+    }
 }
 
-fun ehIgual(s: String): Boolean {
-	return s.equals(palavraSecreta)
-}
+fun verificarEntrada(entrada: String): Boolean {
+    val s = entrada.replace(" ", "")
+    val regex = "[@!#$%^&*()/0-9]".toRegex()
 
-fun temLetrasEmComum(s: String): List<String> {
-
-	return s.mapIndexed {i, x -> 
-		if(x == palavraSecreta[i]) {
-			//na ordem certa
-			"<span class=\"green\">" + x + "</span>"
-		} else if(palavraSecreta.contains(x)) {
-			//fora de ordem
-			"<span class=\"yellow\">" + x + "</span>"
-		} else { 
-			"<span>" + x + "</span>"
-		}
-	}
-}
-
-fun verificarEntrada(s: String): Boolean {
 	if(s == "") {
 		window.alert("INFORME UMA PALAVRA, POR FAVOR!")
 		return false
 	} else if(s.length > 5 || s.length < 5) {
 		window.alert("A PALAVRA PRECISA TER SOMENTE 5 LETRAS!")
 		return false
-	} else {
+	} else if(regex.containsMatchIn(s)) {
+        window.alert("A PALAVRA PRECISA TER SOMENTE 5 LETRAS!")
+		return false
+    } else {
 		return true
 	}	
 }
 
-fun imprimirPalavra(palavra: String): String {
-
-	val letrasEmComum = temLetrasEmComum(palavra)
-	val novaPalavra = letrasEmComum.toString().replace(",", " ")
-	return "<div class=\"jogadas\"><h1> " + novaPalavra.uppercase() + "</h1></div>"
+fun imprimirResultado(resultado: String, tempo: Int) {
+    window.setTimeout(handler = { 
+        area!!.innerHTML +=  resultado
+    }, timeout = tempo)
+    
+    window.setTimeout(handler = { 
+        area!!.innerHTML =  " " 
+    }, timeout = tempo + 2000)     
 }
 
-fun imprimirResultado(acertou: Boolean) {
-	if(acertou) {
-		window.setTimeout(handler = { 
-			area!!.innerHTML +=  "<h1>Você acertou!</h1>"
-		}, timeout = 1000)
-		window.setTimeout(handler = { 
-			area!!.innerHTML =  " " 
-		}, timeout = 3000)
-	} else {
-		window.setTimeout(handler = { 
-			area!!.innerHTML +=  "<h1>Você errou!</h1>"
-		}, timeout = 1000)
-		window.setTimeout(handler = { 
-			area!!.innerHTML =  " " 
-		}, timeout = 3000)
-	}
-}
-
-fun verificarPalavra(s: String): Boolean {
-
-	val result = ehIgual(s)
-	area!!.innerHTML += imprimirPalavra(s)
-	return result
-}
-
-fun fimDeJogo(pontuacao: Int): String {
-	return """
-		<section class="vencendor">
-			<h1> Você ganhou! </h1>
-			<form method="POST" action="salvar_pontuacao">
-				Nome: <input name="nome_usuario" type="text">
-				<input name ="pontos" type="hidden" value="${pontuacao}"> 
-				<input type="submit" value="Enviar">
-			</form>
-		</section>
-	"""
+fun imprimirFimDeJogo(s: String, tempo: Int) {
+    window.setTimeout(handler = { 
+        area!!.innerHTML +=  s
+    }, timeout = tempo)
 }
 
 @JsName("jogar")
 fun jogar() {
 
 	val entrada = document.getElementById("entrada") as HTMLInputElement
-	val palavra = entrada.value
+	val palavraAtual: String = entrada.value
 	entrada.value = ""
 	
-	if(verificarEntrada(palavra)) {
-		if(verificarPalavra(palavra.lowercase())) {
-			imprimirResultado(true)
-			proximaRodada(chances*100)
-	
+	if(verificarEntrada(palavraAtual)) {
+
+        area!!.innerHTML += partida.palavraModificada(palavraAtual)
+
+        val palavraCerta: Boolean = partida.verificarPalavra(palavraAtual.lowercase())
+        val msgResultado: String = partida.acertou(palavraCerta)
+
+		if(palavraCerta) {
+            imprimirResultado(msgResultado, 1000)
+			partida.proximaRodada(partida.chances*100)
+		} else if(partida.chances == 1) {
+            imprimirResultado(msgResultado, 1000)
+			partida.proximaRodada(partida.chances*0)
 		} else {
-			chances--
-		}
+            partida.chances--
+        }
 
-		if(chances == 0) {
-			imprimirResultado(false)
-			proximaRodada(chances)
-		}
-
-		console.log("rodada atual: " + rodada)
-		if(rodada == 2) {
-			console.log("fim de jogo! " + pontucaoTotal)
-
-			window.setTimeout(handler = { 
-				area!!.innerHTML += fimDeJogo(pontucaoTotal)
-			}, timeout = 4000)	
+        if(partida.rodadas == 0) {
+            console.log("pontuacao total: " + partida.pontuacaoTotal)
+            imprimirFimDeJogo(partida.fimDeJogo(), 4000)
 		}
 	}	
 }	
